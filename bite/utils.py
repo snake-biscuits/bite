45c27264a38d2be8037c3d902c508c635d1942db
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2025-06-01 17:27:01 +1000

    (utils) added `rgb_to_rgba`

--- bite/utils.py
+++ bite/utils.py
@@ -2,6 +2,8 @@ import io
 import struct
 from typing import Any, List, Union
 
+import numpy as np
+
 
 def read_struct(stream: io.BytesIO, format_: str) -> Union[Any, List[Any]]:
     byte_size = struct.calcsize(format_)
@@ -15,3 +17,10 @@ def read_struct(stream: io.BytesIO, format_: str) -> Union[Any, List[Any]]:
 
 def write_struct(stream: io.BytesIO, format_: str, *args):
     stream.write(struct.pack(format_, *args))
+
+
+def rgb_to_rgba(rgb: bytes) -> bytes:
+    rgb_array = np.frombuffer(rgb, dtype=np.uint8)
+    rgb_pixels = rgb_array.reshape(rgb_array.size // 3, 3)
+    rgba = np.insert(rgb_pixels, 3, 0xFF, axis=1).flatten()
+    return rgba.tobytes()
