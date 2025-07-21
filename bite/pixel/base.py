f334924830608685dfc06ebb7194aebc0f9dfd95
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2025-07-22 09:22:50 +1000

    (pixel) making plans

--- bite/pixel/base.py
+++ bite/pixel/base.py
@@ -14,14 +14,14 @@ class Type(enum.Enum):
 
 # TODO: dtype lookup table
 # -- match to names & bit sizes
-# -- UINT can be used agnostic of
+# -- UINT can be used agnostic of bit length
 
 
 # TODO: generate PixelFormat -> PixelFormat converters
 # -- using numpy to do array operations
+# -- reordering based on name
 
 
-# TODO: channel component types (to handle RGBA_8888_UINT etc.)
 class Format:
     channels: Dict[str, int]
     # ^ [("channel", num_bits)]
@@ -30,9 +30,10 @@ class Format:
     # TODO: also accept a custom class
     # -- e.g. UF16 (5-bit exponent, 11-bit mantissa)
     # TODO: metadata
-    # -- colour space
+    # -- colour_space: str  # enum?
     # -- is_hdr: bool
 
+    # TODO: ask for a name, and generate if None
     def __init__(self, dtype=np.uint8, **channels):
         self.channels = channels
         self.dtype = dtype
@@ -62,6 +63,7 @@ class Format:
     @classmethod
     def from_name(cls, name: str) -> Format:
         # TODO: optional args for dtype & metadata
+        # NOTE: won't handle names like RGBA_32323232_FLOAT correctly
         if name.count("_") == 1:
             channels, bits = name.split("_")
         elif name.count("_") == 2:
@@ -72,6 +74,29 @@ class Format:
 
     # TODO: bytes -> pixel array (np.array)
     def array_from_bytes(self, bytes) -> np.array:
-        # b"rgbrgb" -> [[r, g, b], [r, g, b], ...]
-        # perfect for detwiddling
+        # we can do 1 dtype per pixel, or multiple
+        # for twiddling we just want 1 entry per pixel
+        # [[r,g,b], ...] vs. [b"rgb", ...]
+        # should compare the performance cost
         ...
+
+
+# copied from `view`: RGB_888 -> RGBA_8888
+def rgb_to_rgba(rgb: bytes) -> bytes:
+    rgb_array = np.frombuffer(rgb, dtype=np.uint8)
+    rgb_pixels = rgb_array.reshape(rgb_array.size // 3, 3)
+    rgba = np.insert(rgb_pixels, 3, 0xFF, axis=1).flatten()
+    return rgba.tobytes()
+
+
+# ^ good template for conversion
+# - convert to array of dtypes
+# -- np.frombuffer
+# - reorder channel(s) [optional]
+# -- dest[0::4] = source[0::3]
+# - array of pixels [add channel(s) prerequisite]
+# -- array.reshape
+# - add channel(s) [optional]
+# -- np.insert
+# - convert to bytes
+# -- return out.flatten().tobytes()
