e832822073885ea63653ae4fa2f7ff4d5196c6cc
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2025-09-25 20:09:07 +1000

    thinking about colour spaces

--- bite/pixel/__init__.py
+++ bite/pixel/__init__.py
@@ -4,5 +4,11 @@ __all__ = [
 
 from . import base
 from . import convert
+# TODO: colour spaces (YUV, Oklab)
+# -- https://en.wikipedia.org/wiki/Y%E2%80%B2UV
+# -- https://en.wikipedia.org/wiki/Oklab_color_space
+# NOTE: CSS Color Module Level 5 has custom colour spaces
+# -- https://drafts.csswg.org/css-color-5
+# -- css parsing & defaults will link us to svg_tool
 
 from .base import Format
